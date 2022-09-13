b4551571efefef0b5092d130b860fde0b087ade0
hubert <hanwenhui520@gmail.com>
2022-09-13 09:09:16 +0800

    修改源码下载地址

--- 更新代码.py
+++ 更新代码.py
@@ -9,7 +9,7 @@ console = Console(color_system="256")
 
 def get_data():
     with console.status('[#37E2D5]更新代码中...') as s:
-        url = "https://github.com/glorydreamtop/ams-py/archive/refs/heads/main.zip"
+        url = "https://gitee.com/glorydream/ams-py/repository/archive/main.zip"
         response = requests.get(url)
     return url, response.content
  
